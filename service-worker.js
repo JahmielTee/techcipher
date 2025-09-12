27f6abdab29fd1455b00903ee72466e2184ecf85
JahmielTee <rapsomeomotola@gmail.com>
2025-09-13 00:29:50 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,38 @@
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open('techcipher-cache').then(cache => {
-      return cache.addAll([
-        './techcipher.html',
-        './manifest.json',
-        './service-worker.js',
-        './icon-192.png',
-        './icon-512.png'
-      ]);
-    })
+const CACHE_NAME = 'royalsmiles-v1';
+const ASSETS = [
+  './index.html','./services.html','./education.html','./contact.html',
+  './manifest.json','./service-worker.js','./icon-192.png','./icon-512.png'
+];
+
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+self.addEventListener('activate', evt => {
+  evt.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(resp => {
+      return resp || fetch(evt.request).then(fetchResp => {
+        caches.open(CACHE_NAME).then(cache => {
+          try{ cache.put(evt.request, fetchResp.clone()); } catch(e){}
+        });
+        return fetchResp;
+      }).catch(()=> {
+        if (evt.request.mode === 'navigate') {
+          return caches.match('./index.html');
+        }
+      });
     })
   );
 });
